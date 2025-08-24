518d1df6c803e209f1e4b644da841a1f2f3683b9
gbmao <gabriel.machado.oli@gmail.com>
2025-08-24 18:00:19 -0300

    aulas 105 e 106

---
+++ Encapsulation/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+
+//        Player player = new Player();
+//        player.name = "Tim";
+//        player.health = 20;
+//        player.weapon = "Sword";
+//
+//        int damage = 10;
+//        player.loseHealth(damage);
+//        System.out.println("remaining health = " + player.healthRemaining());
+//
+//        player.loseHealth(11);
+//        System.out.println("remaining health = " + player.healthRemaining());
+    EnhancedPlayer tim = new EnhancedPlayer("Tim");
+        System.out.println("Initial health is " + tim.healthRemaining());
+    }
+}
