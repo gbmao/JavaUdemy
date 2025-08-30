a1a0c7dd3ff6eb79adc3e9bb8f6d39f40d4d6792
gbmao <gabriel.machado.oli@gmail.com>
2025-08-30 12:39:44 -0300

    feat: intInput: method with 2 parameters to receive user input to chose from menu, can check for possible missMatch errors and exceptions. tested

---
+++ TheArrayListChallenge/src/dev/lpa/utils/InputHelper.java
@@ -0,0 +1,50 @@
+package dev.lpa.utils;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class InputHelper {
+    Scanner scanner = new Scanner(System.in);
+
+    public  int intInput(int a, int b) {
+
+        boolean isInt = true;
+        int response = 0;
+        while(isInt) {
+            isInt = false;
+            try {
+                System.out.print("Digite : ");
+                response = scanner.nextInt();
+                if(response < a || response> b){
+                    throw new  IllegalArgumentException(
+                            String.format("Valor fora do range%nDigite um valor entre %s e %s%n", a, b));
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Isto nao e um numero inteiro");
+                scanner.nextLine();
+                isInt = true;
+            } catch (IllegalArgumentException e){
+                System.out.print(e.getMessage());
+                isInt = true;
+            }
+        }
+
+        return response;
+    }
+
+    public String[] separatedByCommaInput() {
+
+        String response = "";
+        boolean notValid = true;
+         try {
+             System.out.println("Digite os items separados por virgula: ");
+              response = scanner.next();
+         } catch (InputMismatchException e) {
+             System.out.println("Digitou algo errado ai");
+         }
+
+
+
+         return  response.trim().toLowerCase().split(",");
+    }
+}
