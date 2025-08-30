b52ddb4c12d7061070d21cc267af07ddbee9d633
gbmao <gabriel.machado.oli@gmail.com>
2025-08-30 11:30:45 -0300

    Feat: created constructor for items to be created inside ShoppingList

--- ChatGptExercises/src/com/chatGpt/Main.java
+++ ChatGptExercises/src/com/chatGpt/Main.java
@@ -7,22 +7,25 @@ import java.util.List;
 public class Main {
     public static void main(String[]  args) {
 
-        ArrayList<String> shoppingList = new ArrayList<>();
-        shoppingList.addAll(List.of("milk","bread", "eggs"));
-        System.out.println(shoppingList);
-        shoppingList.add(0,"butter");
-        shoppingList.remove("bread");
-        shoppingList.indexOf("eggs");
-        System.out.println(shoppingList);
-        shoppingList.set(shoppingList.indexOf("eggs"), "cheese");
-        System.out.println(shoppingList);
+//        ArrayList<String> shoppingList = new ArrayList<>();
+//        shoppingList.addAll(List.of("milk","bread", "eggs"));
+//        System.out.println(shoppingList);
+//        shoppingList.add(0,"butter");
+//        shoppingList.remove("bread");
+//        shoppingList.indexOf("eggs");
+//        System.out.println(shoppingList);
+//        shoppingList.set(shoppingList.indexOf("eggs"), "cheese");
+//        System.out.println(shoppingList);
+//
+//        shoppingList.sort(Comparator.naturalOrder());
+//
+//        for (int i = 0; i < shoppingList.size(); i++) {
+//
+//            System.out.println(String.format("%d - %s", i, shoppingList.get(i) ));
+//
+//        }
 
-        shoppingList.sort(Comparator.naturalOrder());
+        int[] nums = new int[] {1, 2, 3, 4, 100};
 
-        for (int i = 0; i < shoppingList.size(); i++) {
-
-            System.out.println(String.format("%d - %s", i, shoppingList.get(i) ));
-
-        }
      }
 }
---
+++ TheArrayListChallenge/src/dev/lpa/model/Item.java
@@ -0,0 +1,26 @@
+package dev.lpa.model;
+
+public class Item {
+    private String name;
+    private String type;
+    private int count;
+
+
+    public Item(String name) {
+        this.name = name;
+        type = "Default";
+        count = 1;
+    };
+
+    public Item(String name, String type, int count) {
+        this.name = name;
+        this.type = type;
+        this.count = count;
+    }
+
+    public void addCount(int n) {
+        count += n;
+    }
+
+
+}
