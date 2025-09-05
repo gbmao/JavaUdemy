93fcf2e2db3b1bd760788506175fa499de6f4046
gbmao <gabriel.machado.oli@gmail.com>
2025-09-05 13:45:52 -0300

    Implement two methos in Isaveable interface write(), takes no arguments and returns a List containing objects of type String. read(), takes a List of type String and doesn't return anything.

--- ImplementingIsaveableInterface/src/com/mine/ISaveable.java
+++ ImplementingIsaveableInterface/src/com/mine/ISaveable.java
@@ -1,4 +1,12 @@
 package com.mine;
 
+import java.util.List;
+
 public interface ISaveable {
+
+    //    write(), takes no arguments and returns a List containing objects of type String.
+    List<String> write();
+
+    //    -  read(), takes a List of type String and doesn't return anything.
+    void read(List<String> list);
 }
