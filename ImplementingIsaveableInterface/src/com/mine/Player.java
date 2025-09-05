60fa2941c0acf3d36376c730318f370cfb216925
gbmao <gabriel.machado.oli@gmail.com>
2025-09-05 14:13:46 -0300

    created write and read but not tested yet

--- ImplementingIsaveableInterface/src/com/mine/Player.java
+++ ImplementingIsaveableInterface/src/com/mine/Player.java
@@ -1,5 +1,6 @@
 package com.mine;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Player implements ISaveable {
@@ -58,14 +59,26 @@ public class Player implements ISaveable {
     //-  write(), same as interface. Return a List of the fields in the order they appear in toString().
     @Override
     public List<String> write() {
-        return List.of();
+        List<String> list = new ArrayList<>();
+        list.add(getName());
+        list.add(String.valueOf(getHitPoints()));
+        list.add(String.valueOf(getStrength()));
+        list.add(getWeapon());
+        return list;
     }
 
     //-  read(), same as interface. Store the values in the List, in the order they appear in toString().
     // Make sure the List is not null and the size() is greater than 0 before storing the values.
     @Override
     public void read(List<String> list) {
-
+        if(list == null || list.size() <= 0) {
+            System.out.println("Not a valid list");
+            return;
+        }
+        this.name = list.get(0);
+        this.hitPoints = Integer.parseInt(list.get(1));
+        this.strength = Integer.parseInt(list.get(2));
+        this.weapon = list.get(3);
     }
     // -  toString(), Players overriding toString() method. It takes no arguments and returns a String in the following format:
     //Player{name='Tim', hitPoints=10, strength=15, weapon='Sword'}
