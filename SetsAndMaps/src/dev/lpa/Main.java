526c4923564def647adaa5bdb31f7528ca89bb55
gbmao <gabriel.machado.oli@gmail.com>
2025-10-13 11:10:50 -0300

    208 union, intersection & difference

--- SetsAndMaps/src/dev/lpa/Main.java
+++ SetsAndMaps/src/dev/lpa/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        List<Contact> emails =  ContactData.getData("email");
+        List<Contact> emails = ContactData.getData("email");
         List<Contact> phones = ContactData.getData("phone");
         printData("Phone List", phones);
         printData("Email List", emails);
@@ -27,7 +27,34 @@ public class Main {
                 "RHood@sherwoodforest.org");
         System.out.println(robindHood);
 
+        Set<Contact> unionAB = new HashSet<>();
+        unionAB.addAll(emailContacts);
+        unionAB.addAll(phoneContacts);
+        printData("(A ∪ B) Union of emails (A) with phones (B)", unionAB);
 
+        Set<Contact> intersectAB = new HashSet<>(emailContacts);
+        intersectAB.retainAll(phoneContacts);
+        printData("(A ∩ B) Intersect emails (A) and phones (B)", intersectAB);
+
+        Set<Contact> intersectBA = new HashSet<>(phoneContacts);
+        intersectBA.retainAll(emailContacts);
+        printData("(B ∩ A) Intersect phones (B) and emails (A)", intersectBA);
+
+        Set<Contact> AMinusB = new HashSet<>(emailContacts);
+        AMinusB.removeAll(phoneContacts);
+        printData("(A - B)  emails (A) - phones (B)", AMinusB);
+
+        Set<Contact> BMinusA = new HashSet<>(phoneContacts);
+        BMinusA.removeAll(emailContacts);
+        printData("(B - A) phones (B) - emails (A) ", BMinusA);
+
+        Set<Contact> symmetricDiff = new HashSet<>(AMinusB);
+        symmetricDiff.addAll(BMinusA);
+        printData("Symmetric Difference: phones and emails", symmetricDiff);
+
+        Set<Contact> symmetricDiff2 = new HashSet<>(unionAB);
+        symmetricDiff2.removeAll(intersectAB);
+        printData("Symmetric Difference: phones and emails", symmetricDiff2);
     }
 
     public static void printData(String header, Collection<Contact> contacts) {
