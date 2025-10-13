0c22b288bae499d74c4d010117c13a154ed494fe
gbmao <gabriel.machado.oli@gmail.com>
2025-10-13 10:31:57 -0300

    207 Unordered Sets HashSet

--- SetsAndMaps/src/dev/lpa/Contact.java
+++ SetsAndMaps/src/dev/lpa/Contact.java
@@ -1,6 +1,7 @@
 package dev.lpa;
 
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 public class Contact {
@@ -39,7 +40,7 @@ public class Contact {
 
     @Override
     public String toString() {
-            return "%s: %s %s".formatted(name, emails, phones);
+        return "%s: %s %s".formatted(name, emails, phones);
     }
 
     public Contact mergeContactData(Contact contact) {
@@ -49,5 +50,36 @@ public class Contact {
         newContact.emails.addAll(contact.emails);
         newContact.phones.addAll(contact.phones);
         return newContact;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Contact contact = (Contact) o;
+        return getName().equals(contact.getName());
+    }
+
+    @Override
+    public int hashCode() {
+        return 33 * getName().hashCode();
+    }
+
+    public void addEmail(String companyName) {
+        String[] names = name.split(" ");
+        String email = "%c%s@%s.com".formatted(name.charAt(0), names[names.length - 1],
+                companyName.replaceAll(" ", "").toLowerCase());
+        if(!emails.add(email)) {
+            System.out.println(name + " already has email " + email);
+        } else {
+            System.out.println(name + " now has email " + email);
+        }
+    }
+
+    public void replaceEmailIfExist(String oldEmail, String newEmail) {
+        if(emails.contains(oldEmail)) {
+            emails.remove(oldEmail);
+            emails.add(newEmail);
+        }
     }
 }
