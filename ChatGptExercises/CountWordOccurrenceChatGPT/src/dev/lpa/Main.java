c1dee00fccf30428e5163909f835a03dd3b8eaf7
gbmao <gabriel.machado.oli@gmail.com>
2025-10-20 20:09:19 -0300

    small exercise from chatGpt

---
+++ ChatGptExercises/CountWordOccurrenceChatGPT/src/dev/lpa/Main.java
@@ -0,0 +1,38 @@
+package dev.lpa;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        printMap(createMap(inputSentence()));
+    }
+    public static String inputSentence() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a sentence: ");
+        String response = scanner.nextLine();
+        return response;
+    }
+
+    public static Map<String, Integer> createMap(String sentence) {
+        String[] words = sentence.split(" ");
+        Map<String, Integer> wordsMapped = new HashMap<>();
+        for (String s : words) {
+            if (!wordsMapped.containsKey(s)) {
+                wordsMapped.put(s, 1);
+            } else {
+                wordsMapped.put(s, wordsMapped.get(s) +1);
+            }
+        }
+
+        return wordsMapped;
+    }
+
+    public static void printMap(Map<String, Integer> words) {
+        System.out.println("Word count:");
+        words.forEach((k,v) -> System.out.println(k + " -> " + v));
+    }
+
+}
