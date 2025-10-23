2d4a83ae7bd3afb029ba31d996247bfb0b193805
gbmao <gabriel.machado.oli@gmail.com>
2025-10-23 17:22:06 -0300

    227. Final Modifier

---
+++ 07_Mutability_Final_Immut/FinalExplored/src/consumer/especific/ChildClass.java
@@ -0,0 +1,22 @@
+package consumer.especific;
+
+import dev.lpa.generic.BaseClass;
+
+public class ChildClass extends BaseClass {
+
+    @Override
+    protected void optionalMethod() {
+
+        System.out.println("[Child:optionalMethod] EXTRA Stuff Here");
+        super.optionalMethod();
+    }
+
+//    @Override
+//    public void recommendedMethod() {
+//
+//        System.out.println("[Child:recommendedMethod]: I'll do things my way");
+//        optionalMethod();
+//    }
+
+
+}
---
+++ 07_Mutability_Final_Immut/FinalExplored/src/dev/lpa/Main.java
@@ -0,0 +1,28 @@
+package dev.lpa;
+
+import consumer.especific.ChildClass;
+import dev.lpa.generic.BaseClass;
+
+public class Main {
+    public static void main(String[] args) {
+
+        BaseClass parent = new BaseClass();
+        ChildClass child = new ChildClass();
+        BaseClass childReferredToAsBase = new ChildClass();
+
+        parent.recommendedMethod();
+        System.out.println("--------------");
+        childReferredToAsBase.recommendedMethod();
+        System.out.println("--------------");
+        child.recommendedMethod();
+
+        System.out.println("--------------");
+        parent.recommendedStatic();
+        System.out.println("--------------");
+        childReferredToAsBase.recommendedStatic();
+        System.out.println("--------------");
+        child.recommendedStatic();
+
+    }
+
+}
