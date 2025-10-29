2cf29c395ce8391d19cc4827a89cc15c0b827265
gbmao <gabriel.machado.oli@gmail.com>
2025-10-29 08:53:19 -0300

    239.Record Constructors

--- 07_Mutability_Final_Immut/ConstructorsProject/src/dev/lpa/Main.java
+++ 07_Mutability_Final_Immut/ConstructorsProject/src/dev/lpa/Main.java
@@ -12,5 +12,11 @@ public class Main {
 
         System.out.println("Parent" + parent);
         System.out.println("Child" + child);
+
+        Person joe = new Person("Joe", "01-05-1950");
+        System.out.println(joe);
+
+        Person joeCopy = new Person(joe);
+        System.out.println(joeCopy);
     }
 }
