68e57d25355fb3353386759c3eccb9cce9dbca02
gbmao <gabriel.machado.oli@gmail.com>
2025-10-29 14:14:50 -0300

    Weapon ENUM

---
+++ CodeCHallenge/PirateGameChallenge/src/dev/lpa/game/Weapon.java
@@ -0,0 +1,33 @@
+package dev.lpa.game;
+
+public enum Weapon {
+
+    KNIFE(1,2),
+    MACHETE(2,3),
+    AXE(4,4),
+    SWORD(5,6);
+
+    private final int minLevel;
+
+    private final int hitPoints;
+
+    Weapon(int minLevel, int hitPoints) {
+        this.minLevel = minLevel;
+        this.hitPoints = hitPoints;
+    }
+
+    public int getMinLevel() {
+        return minLevel;
+    }
+
+    public int getHitPoints() {
+        return hitPoints;
+    }
+
+    //Item description
+
+    @Override
+    public String toString() {
+        return this.name().toLowerCase() + " : min lvl: " + this.minLevel + " damage: " + this.hitPoints;
+    }
+}
