e093fddd5292a40f3880d655726a0ceac6c71631
gbmao <gabriel.machado.oli@gmail.com>
2025-11-04 13:46:19 -0300

    253.

---
+++ 08_Streams_PipeLine/StreamIntermediate/src/dev/lpa/Main.java
@@ -0,0 +1,29 @@
+package dev.lpa;
+
+import java.util.Random;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        IntStream.iterate((int) 'A', i-> i <= (int) 'z', i -> i + 1 )
+                .filter(Character::isAlphabetic)
+                .dropWhile(i -> i <= 'E')
+                .takeWhile(i -> i < 'a')
+//                .skip(5)
+//                .filter(i -> Character.toUpperCase(i) > 'E')
+                .forEach(d -> System.out.printf("%c ", d));
+
+        System.out.println();
+
+        Random random = new Random();
+
+        Stream.generate(() -> random.nextInt((int)'A', (int)'Z' + 1))
+                .limit(50)
+                .distinct()
+                .sorted()
+                .forEach(d -> System.out.printf("%c ",d));
+    }
+}
