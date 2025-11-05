9fabb0722b62cbabcedaa7358b9611d9271db6ae
gbmao <gabriel.machado.oli@gmail.com>
2025-11-05 17:23:53 -0300

    255. Find statistics

---
+++ 08_Streams_PipeLine/StreamTerminal/src/dev/lpa/Seat.java
@@ -0,0 +1,9 @@
+package dev.lpa;
+
+import java.util.Random;
+
+public record Seat(char rowMaker, int seatNumber, boolean isReserved) {
+    public Seat(char rowMaker, int seatNumber) {
+        this(rowMaker, seatNumber, new Random().nextBoolean());
+    }
+}
