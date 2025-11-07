49bd979556be9e2dbe24aee70aebe0224150ab97
gbmao <gabriel.machado.oli@gmail.com>
2025-11-07 17:53:02 -0300

    264. min,max,average and reduce stream

---
+++ 08_Streams_PipeLine/StreamingStudents/src/dev/lpa/MainTerminalOption.java
@@ -0,0 +1,78 @@
+package dev.lpa;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class MainTerminalOption {
+    public static void main(String[] args) {
+
+        Course pymc = new Course("PYMC", "Python MasterClass");
+        Course jmc = new Course("JMC", "Java MasterClass");
+
+
+        List<Student> students =
+                Stream.generate(() -> Student.getRandomStudent(jmc, pymc))
+                        .limit(1000)
+                        .toList();
+
+        int minAge = 21;
+        students.stream()
+                .filter(student -> student.getAge() <= minAge)
+                .findAny()
+                .ifPresentOrElse(s -> System.out.printf("Student %d from %s is %d%n",
+                                s.getStudentId(), s.getCountryCode(), s.getAge()),
+                        () -> System.out.println("Didn't Find anyone under " + minAge));
+
+
+        students.stream()
+                .filter(student -> student.getAge() <= minAge)
+                .findFirst()
+                .ifPresentOrElse(s -> System.out.printf("Student %d from %s is %d%n",
+                                s.getStudentId(), s.getCountryCode(), s.getAge()),
+                        () -> System.out.println("Didn't Find anyone under " + minAge));
+
+        students.stream()
+                .filter(student -> student.getAge() <= minAge)
+                .min(Comparator.comparing(Student::getAge))
+                .ifPresentOrElse(s -> System.out.printf("Student %d from %s is %d%n",
+                                s.getStudentId(), s.getCountryCode(), s.getAge()),
+                        () -> System.out.println("Didn't Find anyone under " + minAge));
+
+        students.stream()
+                .filter(student -> student.getAge() <= minAge)
+                .max(Comparator.comparing(Student::getAge))
+                .ifPresentOrElse(s -> System.out.printf("Student %d from %s is %d%n",
+                                s.getStudentId(), s.getCountryCode(), s.getAge()),
+                        () -> System.out.println("Didn't Find anyone under " + minAge));
+
+        students.stream()
+                .filter(student -> student.getAge() <= minAge)
+                .mapToInt(Student::getAge)
+                .average()
+                .ifPresentOrElse(a -> System.out.printf("Avg age under 21: %.2f%n", a),
+                        () -> System.out.println("Didn't find anyone under " + minAge));
+
+
+        students.stream()
+                .filter(student -> student.getAge() <= minAge)
+                .map(Student::getCountryCode)
+                .distinct()
+                .reduce((a, b) -> String.join(",", a, b))
+                .ifPresentOrElse(System.out::println,
+                        () -> System.out.println("None found"));
+
+        students.stream()
+                .map(Student::getCountryCode)
+                .distinct()
+                .map(l -> String.join(",", l))
+                .filter(l -> l.contains("AU"))
+                .findAny()
+                .ifPresentOrElse(System.out::println,
+                        () -> System.out.println("Missing AU"));
+    }
+
+
+}
