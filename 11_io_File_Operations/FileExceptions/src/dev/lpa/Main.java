42107a8199130ba835c8a2dca2f8c08f4eea0552
gbmao <gabriel.machado.oli@gmail.com>
2025-11-26 09:30:36 -0300

    298. try-with-resource in java IO

--- 11_io_File_Operations/FileExceptions/src/dev/lpa/Main.java
+++ 11_io_File_Operations/FileExceptions/src/dev/lpa/Main.java
@@ -1,7 +1,6 @@
 package dev.lpa;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -12,7 +11,7 @@ public class Main {
 
         String fileName = "testing.csv";
 
-        testFile(fileName);
+        testFile2(null);
 
         File file = new File(fileName);
         if (!file.exists()) {
@@ -25,11 +24,41 @@ public class Main {
 
     private static void testFile(String fileName) {
         Path path = Paths.get(fileName);
+        FileReader reader = null;
         try {
-            List<String> lines = Files.readAllLines(path);
+//            List<String> lines = Files.readAllLines(path);
+            reader = new FileReader(fileName);
         } catch (IOException e) {
             throw new RuntimeException(e);
         } finally {
+            if(reader != null) {
+                try {
+                    reader.close();
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+            System.out.println("Maybe I'd log something either way...");
+        }
+        System.out.println("File exists and able to use as a resource");
+    }
+
+
+    private static void testFile2(String fileName) {
+
+        try (FileReader reader = new FileReader(fileName)) {
+        } catch (FileNotFoundException e) {
+            System.out.println("File '" + fileName + "' does not exist");
+            throw new RuntimeException(e);
+        } catch (NullPointerException | IllegalArgumentException badData) {
+            System.out.println("User has added bad data " + badData.getMessage());
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (Exception e) {
+            System.out.println("Something unrelated and unexpected happened");
+        }
+        finally {
             System.out.println("Maybe I'd log something either way...");
         }
         System.out.println("File exists and able to use as a resource");
