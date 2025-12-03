51c97adb158e4297fca487ee1d5074209d0b5596
gbmao <gabriel.machado.oli@gmail.com>
2025-12-03 10:20:05 -0300

    312. File operations Rename,Copy,Delete

---
+++ 11_io_File_Operations/ManagingFiles/src/dev/lpa/Main.java
@@ -0,0 +1,43 @@
+package dev.lpa;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        File oldFile = new File("student-activity.json");
+        File newFile = new File("students.json");
+        if(oldFile.exists()) {
+            oldFile.renameTo(newFile);
+            System.out.println("File renamed successfully");
+        } else {
+            System.out.println("File does not exist!");
+        }
+
+//        Path oldPath = Path.of("students.json");
+//        Path newPath = Path.of("files/student-activity.json");
+//
+//        try {
+//            Files.createDirectories(newPath.subpath(0, newPath.getNameCount() - 1));
+//
+//            Files.move(oldPath, newPath);
+//            System.out.println(oldPath + " moved (renamed to) --> " + newPath);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+
+
+        Path fileDir = Path.of("files");
+        Path resourceDir = Path.of("resources");
+        try {
+            Files.copy(fileDir, resourceDir);
+            System.out.println("Directory copied to " + resourceDir);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
