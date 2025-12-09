c992d968f8e7b2998f16c65f844b180fab1f228a
gbmao <gabriel.machado.oli@gmail.com>
2025-12-09 10:37:03 -0300

    321. deadlocks

--- 12_Multithreading_Concurrency/Synchronization/src/dev/lpa/BankAccount.java
+++ 12_Multithreading_Concurrency/Synchronization/src/dev/lpa/BankAccount.java
@@ -4,8 +4,25 @@ public class BankAccount {
 
     private double balance;
 
-    public BankAccount(double balance) {
+    private String name;
+
+    private final Object lockName = new Object();
+    private final Object lockBalance = new Object();
+
+    public BankAccount(String name, double balance) {
         this.balance = balance;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        synchronized (lockName) {
+            this.name = name;
+            System.out.println("Updated name : " + name);
+        }
     }
 
     public double getBalance() {
@@ -21,11 +38,23 @@ public class BankAccount {
             throw new RuntimeException(e);
         }
 
-        synchronized (this) {
+        synchronized (lockBalance) {
             double origBalance = balance;
             balance += amount;
             System.out.printf("STARTING BALANCE: %.0f, DEPOSIT (%.0f)" +
                     " : NEW BALANCE = %.0f%n", origBalance, amount, balance);
+        }
+        addPromoDollars(amount);
+    }
+
+
+    private void addPromoDollars(double amount) {
+
+        if (amount >= 5000) {
+            synchronized (lockBalance) {
+                System.out.println("Congratulations, you earned a promotional deposit.");
+                balance += 25;
+            }
         }
     }
 
@@ -47,4 +76,5 @@ public class BankAccount {
                     ": INSUFFICIENT FUNDS!", origBalance, amount);
         }
     }
+
 }
