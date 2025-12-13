60327f2b61943585cc2cad665a79319b521c6f7b
gbmao <gabriel.machado.oli@gmail.com>
2025-12-13 17:07:50 -0300

    336. Thread

--- 12_Multithreading_Concurrency/ConsumerProducer/src/dev/lpa/Main.java
+++ 12_Multithreading_Concurrency/ConsumerProducer/src/dev/lpa/Main.java
@@ -1,39 +1,64 @@
 package dev.lpa;
 
 import java.util.Random;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 class MessageRepository {
 
     private String message;
     private boolean hasMessage = false;
 
+    private final Lock lock = new ReentrantLock();
 
-    public synchronized String read() {
+    public String read() {
 
-        while (!hasMessage) {
-
+        if (lock.tryLock()) {
             try {
-                wait();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+                while (!hasMessage) {
+                    try {
+                        Thread.sleep(500);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                hasMessage = false;
+            } finally {
+                lock.unlock();
             }
+        } else {
+            System.out.println("** read blocked " + lock);
+            hasMessage = false;
         }
-        hasMessage = false;
-        notifyAll();
         return message;
     }
 
-    public synchronized void write(String message) {
+    public  void write(String message) {
 
-        while (hasMessage) {
-            try {
-                wait();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
+        try {
+
+
+            if (lock.tryLock(3, TimeUnit.SECONDS)) {
+                try {
+                    while (hasMessage) {
+                        try {
+                            Thread.sleep(500);
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    hasMessage = true;
+                } finally {
+                    lock.unlock();
+                }
+            } else {
+                System.out.println("** write blocked " + lock);
+                hasMessage = true;
             }
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
         }
-        hasMessage = true;
-        notifyAll();
         this.message = message;
 
 
@@ -58,7 +83,7 @@ class MessageWriter implements Runnable {
     @Override
     public void run() {
 
-        Random  random = new Random();
+        Random random = new Random();
         String[] lines = text.split("\n");
 
         for (int i = 0; i < lines.length; i++) {
@@ -107,9 +132,25 @@ public class Main {
 
         MessageRepository messageRepository = new MessageRepository();
 
-        Thread reader = new Thread(new MessageReader(messageRepository));
-        Thread writer = new Thread(new MessageWriter(messageRepository));
+        Thread reader = new Thread(new MessageReader(messageRepository), "Reader");
+        Thread writer = new Thread(new MessageWriter(messageRepository),"Writer");
 
+
+        writer.setUncaughtExceptionHandler((thread, exc) -> {
+            System.out.println("Writer hadexception: " + exc);
+            if (reader.isAlive()) {
+                System.out.println("Going to interrupt the reader");
+                reader.interrupt();
+            }
+        });
+
+        reader.setUncaughtExceptionHandler((thread, exc) -> {
+            System.out.println("Reader hadexception: " + exc);
+            if (writer.isAlive()) {
+                System.out.println("Going to interrupt the reader");
+                writer.interrupt();
+            }
+        });
         reader.start();
         writer.start();
     }
---
+++ 12_Multithreading_Concurrency/Executors/src/dev/lpa/Main.java
@@ -0,0 +1,121 @@
+package dev.lpa;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+
+class ColorThreadFactory implements ThreadFactory {
+
+
+    private String threadName;
+
+    public ColorThreadFactory(ThreadColor color) {
+        this.threadName = color.name();
+    }
+
+    @Override
+    public Thread newThread(Runnable r) {
+
+        Thread thread = new Thread(r);
+        thread.setName(threadName);
+        return thread;
+    }
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        var blueExecutor = Executors.newSingleThreadExecutor(
+                new ColorThreadFactory(ThreadColor.ANSI_BLUE));
+        blueExecutor.execute(Main::countDown);
+        blueExecutor.shutdown();
+
+        boolean isDone = false;
+        try {
+            isDone = blueExecutor.awaitTermination(500, TimeUnit.SECONDS);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        if(isDone) {
+            System.out.println("Blue is finished, starting yellow");
+
+
+            var yellowExecutor = Executors.newSingleThreadExecutor(
+                    new ColorThreadFactory(ThreadColor.ANSI_YELLOW));
+            yellowExecutor.execute(Main::countDown);
+            yellowExecutor.shutdown();
+            try {
+                isDone = yellowExecutor.awaitTermination(500,TimeUnit.SECONDS);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            if(isDone) {
+                System.out.println("Yellow is finished starting red");
+                var redExecutor = Executors.newSingleThreadExecutor(
+                        new ColorThreadFactory(ThreadColor.ANSI_RED));
+                redExecutor.execute(Main::countDown);
+                redExecutor.shutdown();
+                try {
+                    isDone = redExecutor.awaitTermination(500, TimeUnit.SECONDS);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                if (isDone) System.out.println("Everything is finished!");
+            }
+        }
+    }
+
+    public static void notMain(String[] args) {
+
+        Thread blue = new Thread(
+                Main::countDown, ThreadColor.ANSI_BLUE.name());
+        Thread yellow = new Thread(
+                Main::countDown, ThreadColor.ANSI_YELLOW.name());
+        Thread red = new Thread(
+                Main::countDown, ThreadColor.ANSI_RED.name());
+
+
+        blue.start();
+
+        try {
+            blue.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        yellow.start();
+        try {
+            yellow.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        red.start();
+
+
+        try {
+            red.join();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void countDown() {
+
+        String threadName = Thread.currentThread().getName();
+        var threadColor = ThreadColor.ANSI_RESET;
+        try {
+            threadColor = threadColor.valueOf(threadName.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            // user my pass a bad color name, will just ignore this error
+        }
+
+        String color = threadColor.color();
+        for (int i = 20; i >= 0; i--) {
+            System.out.println(color + " " +
+                    threadName.replace("ANSI_", "") + " " + i);
+        }
+
+    }
+}
