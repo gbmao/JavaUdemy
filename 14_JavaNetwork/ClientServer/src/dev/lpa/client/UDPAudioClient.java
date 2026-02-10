5900d047469f12ac25cfb4ae6f313700f4f17ae5
gbmao <gabriel.machado.oli@gmail.com>
2026-02-10 20:39:17 -0300

    393. UDPServer

--- 14_JavaNetwork/ClientServer/src/dev/lpa/client/UDPAudioClient.java
+++ 14_JavaNetwork/ClientServer/src/dev/lpa/client/UDPAudioClient.java
@@ -1,9 +1,11 @@
 package dev.lpa.client;
 
+import javax.sound.sampled.*;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketException;
 
 public class UDPAudioClient {
 
@@ -12,14 +14,39 @@ public class UDPAudioClient {
 
     public static void main(String[] args) {
 
-        try(DatagramSocket clientSocket = new DatagramSocket()) {
+        try (DatagramSocket clientSocket = new DatagramSocket()) {
 
             byte[] audioFileName = "AudioClip.wav".getBytes();
             DatagramPacket packet1 = new DatagramPacket(audioFileName,
                     audioFileName.length, InetAddress.getLocalHost(), SERVER_PORT);
             clientSocket.send(packet1);
-        } catch (IOException e) {
+            playStreamedAudio(clientSocket);
+        } catch (IOException | LineUnavailableException e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    private static void playStreamedAudio(DatagramSocket clientSocket) throws SocketException, LineUnavailableException {
+
+        clientSocket.setSoTimeout(2000);
+        AudioFormat format = new AudioFormat(22050, 16,
+                1, true, false);
+        DataLine.Info info = new DataLine.Info(SourceDataLine.class, format);
+        SourceDataLine line = (SourceDataLine) AudioSystem.getLine(info);
+        line.open();
+        line.start();
+
+        byte[] buffer = new byte[PACKET_SIZE];
+        while (true) {
+            try {
+                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                clientSocket.receive(packet);
+                line.write(buffer,0, packet.getLength());
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+                break;
+            }
+        }
+        line.close();
     }
 }
--- 14_JavaNetwork/ClientServer/src/dev/lpa/server/UDPPacketServer.java
+++ 14_JavaNetwork/ClientServer/src/dev/lpa/server/UDPPacketServer.java
@@ -1,5 +1,7 @@
 package dev.lpa.server;
 
+import jdk.dynalink.StandardOperation;
+
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
@@ -7,6 +9,12 @@ import java.io.File;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.concurrent.TimeUnit;
 
 public class UDPPacketServer {
     private static final int PORT = 5000;
@@ -34,6 +42,45 @@ public class UDPPacketServer {
                 System.out.println(e.getMessage());
             }
 
+            sendDataToClient(audioFileName, serverSocket, clientPacket);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    private static void sendDataToClient(String file, DatagramSocket serverSocket,
+                                         DatagramPacket clientPacket) {
+
+        ByteBuffer buffer = ByteBuffer.allocate(PACKET_SIZE);
+
+        try (FileChannel fileChannel = FileChannel.open(Paths.get(file),
+                StandardOpenOption.READ);
+        ) {
+            InetAddress clientIP = clientPacket.getAddress();
+            int clientPort = clientPacket.getPort();
+
+            while (true) {
+                buffer.clear();
+                if (fileChannel.read(buffer) == -1) {
+                    break;
+                }
+
+                buffer.flip();
+
+                while (buffer.hasRemaining()) {
+                    byte[] data = new byte[buffer.remaining()];
+                    buffer.get(data);
+                    DatagramPacket packet = new DatagramPacket(data,
+                            data.length, clientIP, clientPort);
+                    serverSocket.send(packet);
+                }
+
+                try {
+                    TimeUnit.MILLISECONDS.sleep(22);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
