a20d2097f86749248e1315035d769e4137d1792d
gbmao <gabriel.machado.oli@gmail.com>
2026-02-11 20:41:03 -0300

    URI basics

---
+++ 14_JavaNetwork/HttpProject/src/dev/lpa/URIBasics.java
@@ -0,0 +1,36 @@
+package dev.lpa;
+
+import java.net.URI;
+
+public class URIBasics {
+    public static void main(String[] args) {
+
+    }
+
+    private static void print(URI uri) {
+
+        System.out.printf("""
+                ---------------------------------------------
+                [scheme:]scheme-specific-part[#fragment]
+                ---------------------------------------------
+                Scheme: %s
+                Scheme-specific part: %s
+                 Authority: %s
+                    User info: %s
+                    Host: %s
+                    Port: %d
+                    Path: %s
+                    Query: %s
+                Fragment: %s
+                """,
+                uri.getScheme(),
+                uri.getSchemeSpecificPart(),
+                uri.getAuthority(),
+                uri.getUserInfo(),
+                uri.getHost(),
+                uri.getPath(),
+                uri.getQuery(),
+                uri.getFragment()
+                );
+    }
+}
