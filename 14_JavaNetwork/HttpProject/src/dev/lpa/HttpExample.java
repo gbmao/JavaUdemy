87b2d69a4e34b33faa0aa6f9ca51e5277fe3144b
gbmao <gabriel.machado.oli@gmail.com>
2026-02-15 11:05:07 -0300

    398. simple java http server

--- 14_JavaNetwork/HttpProject/src/dev/lpa/HttpExample.java
+++ 14_JavaNetwork/HttpProject/src/dev/lpa/HttpExample.java
@@ -14,7 +14,8 @@ public class HttpExample {
     public static void main(String[] args) {
 
         try {
-            URL url = new URL("https://example.com/extra");
+//            URL url = new URL("https://example.com/extra");
+            URL url = new URL("http://localhost:8080");
             HttpURLConnection connection = (HttpURLConnection)  url.openConnection();
             connection.setRequestMethod("GET");
             connection.setRequestProperty("User-Agent","Chrome");
