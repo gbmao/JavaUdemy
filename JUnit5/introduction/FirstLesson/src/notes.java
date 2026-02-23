ae02a07e5d95b249b59c5419ab6f4fdb81bc8fcb
gbmao <gabriel.machado.oli@gmail.com>
2026-02-23 14:04:28 -0300

    small annotations

---
+++ JUnit5/introduction/FirstLesson/src/notes.java
@@ -0,0 +1,17 @@
+
+//F.I.R.S.T.
+
+//FAST - unit test small peaces of code that's perform small tasks, them runs fast
+
+//Independent - Uni9t test are independent
+
+// Repeatable - Unit tests are repeatable - it should be repeatable and the result shoulde be the same on all
+// environment
+
+//Self-validating - Unit test validates itself
+
+// Thorough & Timely - Cover edge cases
+
+
+public class notes {
+}
