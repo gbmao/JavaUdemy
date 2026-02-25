193fda65bbbf24538a3801d37f2aa94515b90697
gbmao <gabriel.machado.oli@gmail.com>
2026-02-25 19:51:09 -0300

    Exemplo

--- JUnit5/introduction/CalculatorMavenProject/src/test/java/MethodsOrderedByMethodIndexTest.java
+++ JUnit5/introduction/CalculatorMavenProject/src/test/java/MethodsOrderedByMethodIndexTest.java
@@ -1,33 +1,41 @@
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Order;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class MethodsOrderedByMethodIndexTest {
+
+    StringBuilder completed = new StringBuilder();
+
+    @AfterEach
+    void afterEach(){
+        System.out.println("The state of instance object is: " + completed);
+    }
 
     @Order(1)
     @Test
     void testD() {
         System.out.println("Running test D");
+        completed.append("1");
     }
 
     @Order(2)
     @Test
     void testB() {
         System.out.println("Running test B");
+        completed.append("2");
     }
 
     @Order(3)
     @Test
     void testA() {
         System.out.println("Running test A");
+        completed.append("3");
     }
 
     @Order(4)
-    f
     @Test
     void testC() {
         System.out.println("Running test C");
+        completed.append("4");
     }
 }
